68c159737656b67899f2cdcef7d22e417b44f0aa
Christian Helmuth <christian.helmuth@genode-labs.com>
2014-06-20 17:50:48 +0200

    fb_sdl: X11 keycodes for more reliable key map
    Fixes #1187.

--- repos/os/src/drivers/framebuffer/spec/sdl/input.cc
+++ repos/os/src/drivers/framebuffer/spec/sdl/input.cc
@@ -27,114 +27,109 @@
 /**
  * Convert SDL keycode to Genode keycode
  */
-static long convert_keycode(int sdl_keycode)
+static int translate_keysym(SDL_keysym const &keysym)
 {
 	using namespace Input;
 
-	switch (sdl_keycode) {
+	/*
+	 * Translate from X11 keycode to Input::Keycode as X11 and SDL keysyms are
+	 * not what we want.
+	 */
 
-	case SDLK_BACKSPACE:    return KEY_BACKSPACE;
-	case SDLK_TAB:          return KEY_TAB;
-	case SDLK_RETURN:       return KEY_ENTER;
-	case SDLK_PAUSE:        return KEY_PAUSE;
-	case SDLK_ESCAPE:       return KEY_ESC;
-	case SDLK_SPACE:        return KEY_SPACE;
-	case SDLK_QUOTE:        return KEY_APOSTROPHE;
-	case SDLK_HASH:         return KEY_APOSTROPHE;
-	case SDLK_COMMA:        return KEY_COMMA;
-	case SDLK_MINUS:        return KEY_MINUS;
-	case SDLK_PERIOD:       return KEY_DOT;
-	case SDLK_SLASH:        return KEY_SLASH;
-	case SDLK_0:            return KEY_0;
-	case SDLK_1:            return KEY_1;
-	case SDLK_2:            return KEY_2;
-	case SDLK_3:            return KEY_3;
-	case SDLK_4:            return KEY_4;
-	case SDLK_5:            return KEY_5;
-	case SDLK_6:            return KEY_6;
-	case SDLK_7:            return KEY_7;
-	case SDLK_8:            return KEY_8;
-	case SDLK_9:            return KEY_9;
-	case SDLK_SEMICOLON:    return KEY_SEMICOLON;
-	case SDLK_LESS:         return KEY_BACKSLASH;
-	case SDLK_EQUALS:       return KEY_EQUAL;
-	case SDLK_QUESTION:     return KEY_QUESTION;
-	case SDLK_LEFTBRACKET:  return KEY_LEFTBRACE;
-	case SDLK_BACKSLASH:    return KEY_BACKSLASH;
-	case SDLK_RIGHTBRACKET: return KEY_RIGHTBRACE;
-	case SDLK_BACKQUOTE:    return KEY_GRAVE;
-	case SDLK_a:            return KEY_A;
-	case SDLK_b:            return KEY_B;
-	case SDLK_c:            return KEY_C;
-	case SDLK_d:            return KEY_D;
-	case SDLK_e:            return KEY_E;
-	case SDLK_f:            return KEY_F;
-	case SDLK_g:            return KEY_G;
-	case SDLK_h:            return KEY_H;
-	case SDLK_i:            return KEY_I;
-	case SDLK_j:            return KEY_J;
-	case SDLK_k:            return KEY_K;
-	case SDLK_l:            return KEY_L;
-	case SDLK_m:            return KEY_M;
-	case SDLK_n:            return KEY_N;
-	case SDLK_o:            return KEY_O;
-	case SDLK_p:            return KEY_P;
-	case SDLK_q:            return KEY_Q;
-	case SDLK_r:            return KEY_R;
-	case SDLK_s:            return KEY_S;
-	case SDLK_t:            return KEY_T;
-	case SDLK_u:            return KEY_U;
-	case SDLK_v:            return KEY_V;
-	case SDLK_w:            return KEY_W;
-	case SDLK_x:            return KEY_X;
-	case SDLK_y:            return KEY_Y;
-	case SDLK_z:            return KEY_Z;
-	case SDLK_DELETE:       return KEY_DELETE;
+	switch (keysym.scancode) {
 
-	/* arrows + home/end pad */
-	case SDLK_UP:           return KEY_UP;
-	case SDLK_DOWN:         return KEY_DOWN;
-	case SDLK_RIGHT:        return KEY_RIGHT;
-	case SDLK_LEFT:         return KEY_LEFT;
-	case SDLK_INSERT:       return KEY_INSERT;
-	case SDLK_HOME:         return KEY_HOME;
-	case SDLK_END:          return KEY_END;
-	case SDLK_PAGEUP:       return KEY_PAGEUP;
-	case SDLK_PAGEDOWN:     return KEY_PAGEDOWN;
+	case   9: return KEY_ESC;
 
-	/* function keys */
-	case SDLK_F1:           return KEY_F1;
-	case SDLK_F2:           return KEY_F2;
-	case SDLK_F3:           return KEY_F3;
-	case SDLK_F4:           return KEY_F4;
-	case SDLK_F5:           return KEY_F5;
-	case SDLK_F6:           return KEY_F6;
-	case SDLK_F7:           return KEY_F7;
-	case SDLK_F8:           return KEY_F8;
-	case SDLK_F9:           return KEY_F9;
-	case SDLK_F10:          return KEY_F10;
-	case SDLK_F11:          return KEY_F11;
-	case SDLK_F12:          return KEY_F12;
-	case SDLK_F13:          return KEY_F13;
-	case SDLK_F14:          return KEY_F14;
-	case SDLK_F15:          return KEY_F15;
+	case  10: return KEY_1;
+	case  11: return KEY_2;
+	case  12: return KEY_3;
+	case  13: return KEY_4;
+	case  14: return KEY_5;
+	case  15: return KEY_6;
+	case  16: return KEY_7;
+	case  17: return KEY_8;
+	case  18: return KEY_9;
+	case  19: return KEY_0;
+	case  20: return KEY_MINUS;
+	case  21: return KEY_EQUAL;
+	case  22: return KEY_BACKSPACE;
+	case  23: return KEY_TAB;
+	case  24: return KEY_Q;
+	case  25: return KEY_W;
+	case  26: return KEY_E;
+	case  27: return KEY_R;
+	case  28: return KEY_T;
+	case  29: return KEY_Y;
+	case  30: return KEY_U;
+	case  31: return KEY_I;
+	case  32: return KEY_O;
+	case  33: return KEY_P;
+	case  34: return KEY_LEFTBRACE;
+	case  35: return KEY_RIGHTBRACE;
+	case  36: return KEY_ENTER;
+	case  37: return KEY_LEFTCTRL;
+	case  38: return KEY_A;
+	case  39: return KEY_S;
+	case  40: return KEY_D;
+	case  41: return KEY_F;
+	case  42: return KEY_G;
+	case  43: return KEY_H;
+	case  44: return KEY_J;
+	case  45: return KEY_K;
+	case  46: return KEY_L;
+	case  47: return KEY_SEMICOLON;
+	case  48: return KEY_APOSTROPHE;
+	case  49: return KEY_GRAVE;
+	case  50: return KEY_LEFTSHIFT;
+	case  51: return KEY_BACKSLASH;
+	case  52: return KEY_Z;
+	case  53: return KEY_X;
+	case  54: return KEY_C;
+	case  55: return KEY_V;
+	case  56: return KEY_B;
+	case  57: return KEY_N;
+	case  58: return KEY_M;
+	case  59: return KEY_COMMA;
+	case  60: return KEY_DOT;
+	case  61: return KEY_SLASH;
+	case  62: return KEY_RIGHTSHIFT;
+	case  63: return KEY_KPASTERISK;
+	case  64: return KEY_LEFTALT;
+	case  65: return KEY_SPACE;
+	case  66: return KEY_CAPSLOCK;
+	case  67: return KEY_F1;
+	case  68: return KEY_F2;
+	case  69: return KEY_F3;
+	case  70: return KEY_F4;
+	case  71: return KEY_F5;
+	case  72: return KEY_F6;
+	case  73: return KEY_F7;
+	case  74: return KEY_F8;
+	case  75: return KEY_F9;
+	case  76: return KEY_F10;
+	case  77: return KEY_NUMLOCK;
+	case  78: return KEY_SCROLLLOCK;
+	case  79: return KEY_KP7;
+	case  80: return KEY_KP8;
+	case  81: return KEY_KP9;
+	case  82: return KEY_KPMINUS;
+	case  83: return KEY_KP4;
+	case  84: return KEY_KP5;
+	case  85: return KEY_KP6;
+	case  86: return KEY_KPPLUS;
+	case  87: return KEY_KP1;
+	case  88: return KEY_KP2;
+	case  89: return KEY_KP3;
+	case  90: return KEY_KP0;
+	case  91: return KEY_KPDOT;
 
-	/* special keys */
-	case SDLK_PRINT:        return KEY_PRINT;
-	case SDLK_SCROLLOCK:    return KEY_SCROLLLOCK;
-	case SDLK_MENU:         return KEY_MENU;
+	case  94: return KEY_102ND;
+	case  95: return KEY_F11;
+	case  96: return KEY_F12;
 
-	/* key state modifier keys */
-	case SDLK_NUMLOCK:      return KEY_NUMLOCK;
-	case SDLK_CAPSLOCK:     return KEY_CAPSLOCK;
-	case SDLK_RSHIFT:       return KEY_RIGHTSHIFT;
-	case SDLK_LSHIFT:       return KEY_LEFTSHIFT;
-	case SDLK_RCTRL:        return KEY_RIGHTCTRL;
-	case SDLK_LCTRL:        return KEY_LEFTCTRL;
-	case SDLK_RALT:         return KEY_RIGHTALT;
-	case SDLK_LALT:         return KEY_LEFTALT;
-	case SDLK_RMETA:        return KEY_RIGHTMETA;
-	case SDLK_LMETA:        return KEY_LEFTMETA;
+	case 104: return KEY_KPENTER;
+	case 105: return KEY_RIGHTCTRL;
+	case 106: return KEY_KPSLASH;
 
 	default:                return 0;
 	}
@@ -157,12 +152,12 @@ static Input::Event wait_for_sdl_event()
 		SDL_GetMouseState(&mx, &my);
 
 	/* determine keycode */
-	long keycode = 0;
+	int keycode = 0;
 	switch (event.type) {
 	case SDL_KEYUP:
 	case SDL_KEYDOWN:
 
-		keycode = convert_keycode(event.key.keysym.sym);
+		keycode = translate_keysym(event.key.keysym);
 		break;
 
 	case SDL_MOUSEBUTTONDOWN:
