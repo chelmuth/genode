88b4957f544bc719e75d97eab60851612d526bf6
Christian Helmuth <christian.helmuth@genode-labs.com>
2022-09-14 15:49:25 +0200

    WIP prefix RPCs in trace/policy/rpc_name

--- repos/os/src/trace/policy/rpc_name/policy.cc
+++ repos/os/src/trace/policy/rpc_name/policy.cc
@@ -27,34 +27,42 @@ size_t log_output(char *dst, char const *log_message, size_t len)
 
 size_t rpc_call(char *dst, char const *rpc_name, Msgbuf_base const &)
 {
+	*dst++ = 'C'; *dst++ = ':';
+
 	size_t len = strlen(rpc_name);
 
 	memcpy(dst, (void*)rpc_name, len);
-	return len;
+	return len + 2;
 }
 
 size_t rpc_returned(char *dst, char const *rpc_name, Msgbuf_base const &)
 {
+	*dst++ = 'R'; *dst++ = ':';
+
 	size_t len = strlen(rpc_name);
 
 	memcpy(dst, (void*)rpc_name, len);
-	return len;
+	return len + 2;
 }
 
 size_t rpc_dispatch(char *dst, char const *rpc_name)
 {
+	*dst++ = 'D'; *dst++ = ':';
+
 	size_t len = strlen(rpc_name);
 
 	memcpy(dst, (void*)rpc_name, len);
-	return len;
+	return len + 2;
 }
 
 size_t rpc_reply(char *dst, char const *rpc_name)
 {
+	*dst++ = 'Y'; *dst++ = ':';
+
 	size_t len = strlen(rpc_name);
 
 	memcpy(dst, (void*)rpc_name, len);
-	return len;
+	return len + 2;
 }
 
 size_t signal_submit(char *dst, unsigned const)
